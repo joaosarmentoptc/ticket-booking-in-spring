33393f4177daeaeeb6ec48a9ecb91ef1f55f43e1
João Sarmento <joao.sarmento@jumia.com>
2024-05-29 16:28:58 +0100

    Splitted into microservices

---
+++ tickets/registrations-service/src/main/java/com/academic/springboot/tickets/RegistrationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.academic.springboot.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationsServiceApplication.class, args);
+	}
+
+}
---
+++ tickets/registrations-service/src/test/java/com/academic/springboot/tickets/RegistrationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.springboot.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
